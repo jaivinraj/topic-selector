7a77e395d4b5697d715e8dc9dd6f4122c8ae370a
Jaivin Raj <jaivinraj@Jaivins-MBP.lan>
2022-05-17 14:46:00 +0100

    Move app to __init__

---
+++ src/topic_selector/__init__.py
@@ -0,0 +1,10 @@
+from dash import Dash, dash_table
+from dash_bootstrap_components import themes
+
+app = Dash(
+    __name__,
+    external_stylesheets=[
+        themes.BOOTSTRAP,
+        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+    ],
+)

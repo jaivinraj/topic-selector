7c009c197ec18c75f8ae04150fac81bc2dedac9f
Jaivin Raj <jaivinraj@Jaivins-MBP.lan>
2022-05-17 15:32:03 +0100

    Clean up main

--- src/topic_selector/main.py
+++ src/topic_selector/main.py
@@ -6,33 +6,7 @@ import pandas as pd
 from topic_selector.components.task_table import table_layout
 from topic_selector.components.selection import selection_layout
 
-# app = Dash(
-#     __name__,
-#     external_stylesheets=[
-#         themes.BOOTSTRAP,
-#         "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
-#     ],
-# )
-
 dashapp.layout = html.Div([selection_layout, table_layout])
-
-# @app.callback(
-#     Output("table-editing-simple-output", "figure"),
-#     Input("table-editing-simple", "data"),
-#     Input("table-editing-simple", "columns"),
-# )
-# def display_output(rows, columns):
-#     df = pd.DataFrame(rows, columns=[c["name"] for c in columns])
-#     return {
-#         "data": [
-#             {
-#                 "type": "parcoords",
-#                 "dimensions": [
-#                     {"label": col["name"], "values": df[col["id"]]} for col in columns
-#                 ],
-#             }
-#         ]
-#     }
 
 
 if __name__ == "__main__":

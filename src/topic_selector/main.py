69d5957519c46b5856f3612ece2e9bade43d4f96
Jaivin Raj <jaivinraj@Jaivins-MBP.lan>
2022-05-17 14:27:17 +0100

    Clean up main file

--- src/topic_selector/main.py
+++ src/topic_selector/main.py
@@ -5,23 +5,7 @@ from topic_selector.components.task_table import task_table
 
 app = Dash(__name__)
 
-# params = ["Weight", "Torque", "Width", "Height", "Efficiency", "Power", "Displacement"]
-
-app.layout = html.Div(
-    [
-        task_table
-        # dash_table.DataTable(
-        #     id="table-editing-simple",
-        #     columns=(
-        #         [{"id": "Model", "name": "Model"}]
-        #         + [{"id": p, "name": p} for p in params]
-        #     ),
-        #     data=[dict(Model=i, **{param: 0 for param in params}) for i in range(1, 5)],
-        #     editable=True,
-        # ),
-        # dcc.Graph(id="table-editing-simple-output"),
-    ]
-)
+app.layout = html.Div([task_table])
 
 
 # @app.callback(
